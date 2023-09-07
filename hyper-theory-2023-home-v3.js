a8e10fea67e997f2b5ae2d88f2f644af9ba13b0b
reeloui <144292117+reeloui@users.noreply.github.com>
2023-09-07 10:34:08 +1000

    Update hyper-theory-2023-home-v3.js
    Trying the original v2 code

--- hyper-theory-2023-home-v3.js
+++ hyper-theory-2023-home-v3.js
@@ -1,56 +1,57 @@
+<!-- Include Matter.js library -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.18.0/matter.min.js" integrity="sha512-5T245ZTH0m0RfONiFm2NF0zcYcmAuNzcGyPSQ18j8Bs5Pbfhp5HP1hosrR8XRt5M3kSRqzjNMYpm2+it/AUX/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script>
-  const C = document.querySelector("#matter-container");
-  const E = Matter.Engine, R = Matter.Render, N = Matter.Runner, B = Matter.Bodies, Cm = Matter.Composite, Bdy = Matter.Body;
-  const engine = E.create();
-  const render = R.create({ element: C, engine, options: { width: C.clientWidth, height: C.clientHeight, background: "transparent", wireframes: false, showAngleIndicator: false } });
+  const matterContainer = document.querySelector("#matter-container");
+  const THICCNESS = 60;
+  const Engine = Matter.Engine, Render = Matter.Render, Runner = Matter.Runner, Bodies = Matter.Bodies, Composite = Matter.Composite, Body = Matter.Body;
+  const engine = Engine.create();
+  const render = Render.create({ element: matterContainer, engine, options: { width: matterContainer.clientWidth, height: matterContainer.clientHeight, background: "transparent", wireframes: false, showAngleIndicator: false } });
   const numBodies = 200;
 
   for (let i = 0; i < numBodies; i++) {
-    let circle = B.circle(Math.random() * C.clientWidth, Math.random() * C.clientHeight, 39, { friction: .1, frictionAir: 0.00001, restitution: 0.3, render: { sprite: { texture: 'https://uploads-ssl.webflow.com/64f024c208db7bbce63d76ba/64f90fa2015b8188d102df39_ballpit-ball-pink-77px.png' } } });
-    let circle2 = B.circle(Math.random() * C.clientWidth, Math.random() * C.clientHeight, 39, { friction: .1, frictionAir: 0.00001, restitution: 0.3, render: { sprite: { texture: 'https://uploads-ssl.webflow.com/64f024c208db7bbce63d76ba/64f90fa26b0566382035e3eb_ballpit-ball-blue-77px.png' } } });
-    let circle3 = B.circle(Math.random() * C.clientWidth, Math.random() * C.clientHeight, 39, { friction: .1, frictionAir: 0.00001, restitution: 0.3, render: { sprite: { texture: 'https://uploads-ssl.webflow.com/64f024c208db7bbce63d76ba/64f90fa21881c505867d1ccd_ballpit-ball-pink-47px.png' } } });
-    let circle4 = B.circle(Math.random() * C.clientWidth, Math.random() * C.clientHeight, 39, { friction: .1, frictionAir: 0.00001, restitution: 0.3, render: { sprite: { texture: 'https://uploads-ssl.webflow.com/64f024c208db7bbce63d76ba/64f90fa2c5e1bf40aaa03bde_ballpit-ball-blue-47px.png' } } });
-    Cm.add(engine.world, circle, circle2, circle3, circle4);
+    let circle = Bodies.circle(Math.random() * matterContainer.clientWidth, Math.random() * matterContainer.clientHeight, 39, { friction: .1, frictionAir: 0.00001, restitution: 0.3, render: { sprite: { texture: 'https://uploads-ssl.webflow.com/64f024c208db7bbce63d76ba/64f12d4a6fa1f35dfc487566_ballpit-ball-logo-77px-2.png' } } });
+    Composite.add(engine.world, circle);
   }
 
-  const ground = B.rectangle(C.clientWidth / 2, C.clientHeight + 60 / 2, 27184, 60, { isStatic: true });
-  const outerContainer = B.rectangle(C.clientWidth * 0.4 + (C.clientWidth * 0.9) / 2, ground.position.y - ground.height * 0.2, C.clientWidth * 0.9 + 150 + 150, C.clientHeight * 0.4 + 150 + 150, { isStatic: true, render: { fillStyle: "transparent", lineWidth: 0 } });
-  const innerContent = B.rectangle(outerContainer.position.x, outerContainer.position.y, C.clientWidth * 0.9, C.clientHeight * 0.4, { isStatic: true, render: { fillStyle: "#ffffff", sprite: { texture: null }, lineWidth: 0 } });
-  const staticObject = B.rectangle(C.clientWidth * 0.4 + (C.clientWidth * 0.9) / 2, ground.position.y - ground.height * 0.2, C.clientWidth * 0.9, C.clientHeight * 0.4, { isStatic: true, render: { fillStyle: "#ffffff", sprite: { texture: null }, lineWidth: 0 } });
-  Cm.add(engine.world, staticObject);
+  const ground = Bodies.rectangle(matterContainer.clientWidth / 2, matterContainer.clientHeight + THICCNESS / 2, 27184, THICCNESS, { isStatic: true });
+  const paddingLeft = 150, paddingTop = 150, paddingRight = 150, paddingBottom = 150;
+  const outerContainer = Bodies.rectangle(matterContainer.clientWidth * 0.4 + (matterContainer.clientWidth * 0.9) / 2, ground.position.y - ground.height * 0.2, matterContainer.clientWidth * 0.9 + paddingLeft + paddingRight, matterContainer.clientHeight * 0.4 + paddingTop + paddingBottom, { isStatic: true, render: { fillStyle: "transparent", lineWidth: 0 } });
+  const innerContent = Bodies.rectangle(outerContainer.position.x, outerContainer.position.y, matterContainer.clientWidth * 0.9, matterContainer.clientHeight * 0.4, { isStatic: true, render: { fillStyle: "#ffffff", sprite: { texture: null }, lineWidth: 0 } });
+  const staticObject = Bodies.rectangle(matterContainer.clientWidth * 0.4 + (matterContainer.clientWidth * 0.9) / 2, ground.position.y - ground.height * 0.2, matterContainer.clientWidth * 0.9, matterContainer.clientHeight * 0.4, { isStatic: true, render: { fillStyle: "#ffffff", sprite: { texture: null }, lineWidth: 0 } });
+  Composite.add(engine.world, staticObject);
 
-  Cm.add(engine.world, [outerContainer, innerContent]);
-  let leftWall = B.rectangle(0 - 60 / 2, C.clientHeight / 2, 60, C.clientHeight * 5, { isStatic: true });
-  let rightWall = B.rectangle(C.clientWidth + 60 / 2, C.clientHeight / 2, 60, C.clientHeight * 5, { isStatic: true });
-  Cm.add(engine.world, [ground, leftWall, rightWall]);
+  Composite.add(engine.world, [outerContainer, innerContent]);
+  let leftWall = Bodies.rectangle(0 - THICCNESS / 2, matterContainer.clientHeight / 2, THICCNESS, matterContainer.clientHeight * 5, { isStatic: true });
+  let rightWall = Bodies.rectangle(matterContainer.clientWidth + THICCNESS / 2, matterContainer.clientHeight / 2, THICCNESS, matterContainer.clientHeight * 5, { isStatic: true });
+  Composite.add(engine.world, [ground, leftWall, rightWall]);
   let mouse = Matter.Mouse.create(render.canvas);
   let mouseConstraint = Matter.MouseConstraint.create(engine, { mouse, constraint: { stiffness: 0.2, render: { visible: false } } });
-  Cm.add(engine.world, mouseConstraint);
+  Composite.add(engine.world, mouseConstraint);
   mouseConstraint.mouse.element.removeEventListener("mousewheel", mouseConstraint.mouse.mousewheel);
   mouseConstraint.mouse.element.removeEventListener("DOMMouseScroll", mouseConstraint.mouse.mousewheel);
-  R.run(render);
-  const runner = N.create();
-  N.run(runner, engine);
+  Render.run(render);
+  const runner = Runner.create();
+  Runner.run(runner, engine);
 
-  function handleResize(C) {
-    render.canvas.width = C.clientWidth;
-    render.canvas.height = C.clientHeight;
-    Bdy.setPosition(ground, Matter.Vector.create(C.clientWidth / 2, C.clientHeight + 60 / 2));
-    Bdy.setPosition(rightWall, Matter.Vector.create(C.clientWidth + 60 / 2, C.clientHeight / 2));
+  function handleResize(matterContainer) {
+    render.canvas.width = matterContainer.clientWidth;
+    render.canvas.height = matterContainer.clientHeight;
+    Matter.Body.setPosition(ground, Matter.Vector.create(matterContainer.clientWidth / 2, matterContainer.clientHeight + THICCNESS / 2));
+    Matter.Body.setPosition(rightWall, Matter.Vector.create(matterContainer.clientWidth + THICCNESS / 2, matterContainer.clientHeight / 2));
   }
 
   function handleResizeStaticObject() {
-    Bdy.setPosition(staticObject, { x: C.clientWidth * 0.4 + (C.clientWidth * 0.9) / 2, y: ground.position.y - ground.height * 0.2 });
+    Matter.Body.setPosition(staticObject, { x: matterContainer.clientWidth * 0.4 + (matterContainer.clientWidth * 0.9) / 2, y: ground.position.y - ground.height * 0.2 });
   }
 
   window.addEventListener("resize", () => {
-    handleResize(C);
+    handleResize(matterContainer);
     handleResizeStaticObject();
   });
 
-  handleResize(C);
+  handleResize(matterContainer);
+  handleResizeStaticObject();
 
   function triggerExplosion() {
     for (let body of engine.world.bodies) {
@@ -58,7 +59,7 @@
         const forceMagnitude = 0.5 * Math.random() + 0.5;
         const angle = Math.random() * 2 * Math.PI;
         const force = { x: forceMagnitude * Math.cos(angle), y: forceMagnitude * Math.sin(angle) };
-        Bdy.applyForce(body, body.position, force);
+        Body.applyForce(body, body.position, force);
       }
     }
   }
@@ -86,6 +87,7 @@
     }).fromTo(targetElement, from, to, duration);
   }
 
+  // Sticky Circle Grow
   $(".sticky-circle_wrap").each(function () {
     createTimeline(
       $(this),
@@ -99,6 +101,8 @@
     );
   });
 
+  // Define backgroundColorFrom, colorFrom, backgroundColorTo, and colorTo variables or provide appropriate values here.
+
   createTimeline(
     $(this),
     $("body, .section.is--nav"),
@@ -109,16 +113,20 @@
     { backgroundColor: backgroundColorTo, color: colorTo },
     1
   );
+
 </script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js"></script>
 <script>
-$("[tr-scroll-toggle='component']").each(function () {
+$("[tr-scroll-toggle='component']").each(function (index) {
+  // get elements
   let component = $(this);
   let lists = component.find("[tr-scroll-toggle='list']");
+  // set item total
   let itemTotal = lists.first().children().length;
   component.find("[tr-scroll-toggle='number-total']").text(itemTotal);
+  // create trigger divs & spacer
   let firstTrigger = component.find("[tr-scroll-toggle='trigger']").first();
   for (let i = 1; i < itemTotal; i++) {
     firstTrigger.clone().appendTo(component);
@@ -126,29 +134,35 @@ $("[tr-scroll-toggle='component']").each(function () {
   let triggers = component.find("[tr-scroll-toggle='trigger']");
   firstTrigger.css("margin-top", "-100vh");
   let trSpacer = $("<div class='tr-scroll-toggle-spacer' style='width: 100%; height: 100vh;'></div>").hide().appendTo(component);
+  // check for min width
   let minWidth = 0;
   let trMinWidth = component.attr("tr-min-width");
   if (trMinWidth !== undefined && trMinWidth !== false) {
     minWidth = +trMinWidth;
   }
+  // main breakpoint
   gsap.matchMedia().add(`(min-width: ${minWidth}px)`, () => {
+    // show spacer
     trSpacer.show();
+    // switch which item is active
     function makeItemActive(activeIndex) {
       component.find("[tr-scroll-toggle='transform-y']").css("transform", `translateY(${activeIndex * -100}%)`);
       component.find("[tr-scroll-toggle='transform-x']").css("transform", `translateX(${activeIndex * -100}%)`);
       component.find("[tr-scroll-toggle='number-current']").text(activeIndex + 1);
-      lists.each(function () {
+      lists.each(function (index) {
         $(this).children().removeClass("is-active");
         $(this).children().eq(activeIndex).addClass("is-active");
       });
     }
     makeItemActive(0);
+    // scroll to trigger div on click of anchor
     let anchorLinks = component.find("[tr-anchors]").children();
     anchorLinks.on("click", function () {
       let myIndex = $(this).index();
       let scrollDistance = triggers.eq(myIndex).offset().top + triggers.eq(myIndex).height() - 1;
       $("html, body").animate({ scrollTop: scrollDistance });
     });
+    // triggers timeline
     triggers.each(function (index) {
       let triggerIndex = index;
       let tl = gsap.timeline({
@@ -177,6 +191,7 @@ $("[tr-scroll-toggle='component']").each(function () {
         tl.from(childItem.find("[tr-item-animation='rotate-from-0']"), { rotation: 0 }, 0);
       });
     });
+    // component timeline
     let tl = gsap.timeline({
       scrollTrigger: {
         trigger: component,
@@ -194,6 +209,7 @@ $("[tr-scroll-toggle='component']").each(function () {
     tl.to(component.find("[tr-section-animation='progress-vertical']"), { height: "100%" }, 0);
     tl.to(component.find("[tr-section-animation='rotate-to-0']"), { rotation: 0 }, 0);
     tl.from(component.find("[tr-section-animation='rotate-from-0']"), { rotation: 0 }, 0);
+    // optional scroll snapping
     if (component.attr("tr-scroll-snap") === "true") {
       let tl2 = gsap.timeline({
         scrollTrigger: {
@@ -213,11 +229,12 @@ $("[tr-scroll-toggle='component']").each(function () {
         tl2.addLabel("trigger" + index);
       });
     }
+    // smaller screen sizes
     return () => {
       trSpacer.hide();
       component.find("[tr-scroll-toggle='transform-y']").css("transform", "translateY(0%)");
       component.find("[tr-scroll-toggle='transform-x']").css("transform", "translateX(0%)");
-      lists.each(function () {
+      lists.each(function (index) {
         $(this).children().removeClass("is-active");
       });
     };
